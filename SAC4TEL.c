4f76f231747405f2eb8265644495988f8bee417c
HTI Sistemas <htisistema@gmail.com>
2023-10-02 19:43:33 -0300

    obj c

--- SAC4TEL.c
+++
@@ -1,9 +0,0 @@
-/*
- * xHarbour build 0.99.70 Intl. (SimpLex)
- * Generated C source code from <SAC4TEL.PRG>
- * Command: C:\xHarbour997\bin\harbour.exe C:\HTI\SISCOM\SAC4TEL.PRG /q /oC:\hti\SISCOM\SAC4TEL.c /M /N -DxHB -DXHB_0990 -DXHB_0991 -DXHB_0992 -DXHB_0993 -DXHB_0994 -DXHB_09941 -DXHB_09950 -DXHB_09951a -DXHB_09951 -DXHB_09951b -DXHB_0996 -DXHB_09960 -DXHB_09961 -DXHB_09961B -DXHB_0997 -DXHB_09970 
- * Created: 2023.10.02 16:10:53 (Borland C++ 5.5.1)
- */
-
-/* Empty source file */
-
