8feb9502ecad85e3f1bcc3fb1c2d2e914ec05532
HTI Sistemas <htisistema@gmail.com>
2024-02-29 08:51:25 -0300

    atualiza240229

--- CLIENTE-NOVO/bkpgdb.bat
+++
@@ -1,9 +0,0 @@
-@ ECHO OFF
-CLS
-@ ECHO .
-@ ECHO Aguarde um momento que estar sendo feito o BACKUP no seu BANCO DE DADOS.....
-DEL siscom.bkp
-gbak -B -user SYSDBA -password masterkey siscom.gdb siscom.bkp
-@ ECHO .
-@ ECHO .
-PAUSE
--- TOOLS/ch/exemplos/icones.ch
+++
@@ -1,2 +0,0 @@
-#define ID_MAINICON 100
-
